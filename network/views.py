d86f52bd2fcc44062ec2f91943ef247f81505c1e
JoeyF92 <jf-9294@protonmail.com>
2023-03-11 19:47:15 +0000

    query refactor part 1

--- network/views.py
+++ network/views.py
@@ -16,7 +16,73 @@ from django.forms.models import model_to_dict
 from django.db.models import Count, Case, When, BooleanField, F, Subquery, OuterRef
 
 
+
 def paginated_post(page_number, user=None, page_type='Index'):
+    #if we're looking on the follow page
+    if page_type == 'Following':
+        # extract the people that the current user is following - using __ to traverse the user foreign key, to retrieve the id
+        following = Follow.objects.filter(user=user).values_list('user_to_follow__id', flat=True)
+        # do a query for all posts, filtering for posts by the user id (traverse the foreign key again)
+        all_posts = Post.objects.filter(user__id__in=following)
+        
+    #else if we're looking at a profile page
+    elif page_type == 'Profile':
+        #do a query for all posts, filtering for posts by the user id (traverse the foreign key again)
+        all_posts = Post.objects.filter(user=user)
+    
+    #else we're looking at the index page      
+    else:
+        all_posts = Post.objects.all()
+    
+    #use annotate + count to work out number of likes per post
+    all_posts = all_posts.annotate(num_likes=Count('likes'),
+        #use subquery to see if user has liked each post - using reverse relation on the user model
+        user_liked=models.Exists(
+            user.liked.filter(id=OuterRef('pk')).values('id')
+        ),
+        #add username and id field, which traversing user
+        username=F('user__username'),
+        userid=F('user__id'),
+        #add is owner field, which is True is current users id matches the posts id
+        is_owner=Case(
+            When(user_id=user.id, then=True),
+            default=False,
+            output_field=BooleanField()
+        )
+        #extract values we want to pass through in response and order by timestamp   
+    ).values('id', 'post', 'username', 'timestamp', 'num_likes', 'user_liked', 'userid', 'edited', 'is_owner').order_by("-timestamp")
+
+    #paginate all_posts and select page requested
+    paginator = Paginator(all_posts, 10)
+    page_obj = paginator.page(page_number)
+    #get list of objects for current page
+    posts = page_obj.object_list
+
+    #convert datetime object into readable string
+    for post in posts:
+        post['timestamp'] = post['timestamp'].strftime('%b %d %Y, %I:%M %p')
+    
+    #create a dictionary to send as response. converting posts to a list
+    data = {
+        'posts' : list(posts),
+        'prev_page': page_obj.has_previous(),
+        'next_page' : page_obj.has_next(),
+        'current_page': page_obj.number,
+        'total_pages': paginator.num_pages
+    }
+
+
+    return data    
+
+
+
+
+
+
+
+
+
+def refactor(page_number, user=None, page_type='Index'):
     #if we're looking on the follow page
     if page_type == 'Following':
         # extract the people that the current user is following - using __ to traverse the user foreign key, to retrieve the id
@@ -215,7 +281,6 @@ def new_post(request):
 
 @login_required
 def likes(request, post_id, action):
-    print('hody')
     user = request.user
     post = Post.objects.get(id=post_id)
     #to dislike post:
@@ -371,6 +436,7 @@ def register(request):
         return render(request, "network/register.html")
 
 #stop being able to click on hearts if not logged in
+#when a follow page is blank?
 #{% if user.is_authenticated %}
 #add some error handling for urls that dont exist, or users not logged in or url parameters not there
 #log in functionality- ie what to see if not logged in, be thorough
